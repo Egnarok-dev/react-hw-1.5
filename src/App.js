9ad18f4c3fa69a6b8ffb2e62b4d40ffb72f4cf99
Egnarok-dev <124812439+Egnarok-dev@users.noreply.github.com>
2023-08-16 23:00:04 +0300

    Add file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Book from './pages/Book';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Book/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
